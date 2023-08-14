9d76b186b74ef7fc078b87644f5fe6711dc2609f
flourdau <lourdaux.florian.pro@gmail.com>
2023-08-14 17:45:54 +0200

    Layout Up2

--- src/components/MyCard/index.jsx
+++ src/components/MyCard/index.jsx
@@ -6,7 +6,7 @@ import './MyCard.css'
 export default function MyCard({ id, title, cover }) {
 	return (
 
-	<Link target="_top" className='portfolio-item' to={ `./product/${ id }`}>
+	<Link reloadDocument className='portfolio-item' to={ `./product/${ id }`}>
 
 		<Card>
 
